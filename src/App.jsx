97b17e7c5664f3087c7fea4b90faabaae11f9f27
dumbus <dumbusx@gmail.com>
2024-04-26 16:23:10 +0300

    feat: add static layout for component MoviePage

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import MovieList from './components/MovieList/MovieList';
+import MoviePage from './components/MoviePage/MoviePage';
 
 const App = () => {
   return (
     <div className='wrapper'>
       <Header />
-      <MovieList />
+      {/* <MovieList /> */}
+      <MoviePage />
       <Footer />
     </div>
   )
