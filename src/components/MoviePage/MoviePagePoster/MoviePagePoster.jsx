8b9cf7ff91ceba68fa73057f21bc2cf96caf3b92
dumbus <dumbusx@gmail.com>
2024-04-30 02:35:03 +0300

    feat: add placeholder images for MoviePagePoster

--- src/components/MoviePage/MoviePagePoster/MoviePagePoster.jsx
+++ src/components/MoviePage/MoviePagePoster/MoviePagePoster.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 
 import './MoviePagePoster.scss';
 
-import basePoster from '../../../assets/poster.webp';
+import posterTemplate from '../../../assets/poster.webp';
 
 const MoviePagePoster = ({ movieData }) => {
   const { posterUrl, name } = movieData;
 
   return (
-    <div className="movie-page__poster_wrapper block">
-      <img
-        className="movie-page__poster_image"
-        src={posterUrl || basePoster}
-        alt={name}
-      />
+    <div className="movie-list__item_poster">
+      <div className="movie-page__poster_wrapper block">
+        <img
+          src={posterTemplate}
+          alt={name}
+          className="movie-page__poster_image movie-page__poster_placeholder"
+        />
+        <img
+          className="movie-page__poster_image movie-page__poster_actual"
+          src={posterUrl || posterTemplate}
+          alt={name}
+        />
+      </div>
     </div>
   );
 };
