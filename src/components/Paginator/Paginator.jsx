a2832e8666e264f095984a0ef9a10eaddff46e1e
dumbus <dumbusx@gmail.com>
2024-12-28 11:31:37 +0300

    fix: fix pagination bugs (fix variable types)

--- src/components/Paginator/Paginator.jsx
+++ src/components/Paginator/Paginator.jsx
@@ -6,39 +6,42 @@ import './Paginator.scss';
 const Paginator = ({ page, pages, listType }) => {
   const navigate = useNavigate();
 
+  const currentPage = Number(page);
+  const totalPages = Number(pages);
+
   const onPageSwitch = (base, offset = 0) => {
     const newPage = base + offset;
 
     navigate(`/movies/${listType}/${newPage}`);
   };
 
-  const renderCenterButtons = (page, pages) => {
+  const renderCenterButtons = (currentPage, totalPages) => {
     const buttons = [];
 
     let first;
     let last;
 
-    if (pages <= 5) {
+    if (totalPages <= 5) {
       first = 1;
-      last = pages;
-    } else if (page <= 3) {
+      last = totalPages;
+    } else if (currentPage <= 3) {
       first = 1;
       last = 3;
-    } else if (page <= pages - 3 && page >= 3) {
-      first = page - 1;
-      last = page + 1;
+    } else if (currentPage <= totalPages - 3 && currentPage >= 3) {
+      first = currentPage - 1;
+      last = currentPage + 1;
     } else {
-      first = pages - 2;
-      last = pages;
+      first = totalPages - 2;
+      last = totalPages;
     }
 
     for (let i = first; i <= last; i++) {
       const button = (
         <button
           key={i}
-          className={`paginator__button ${i === page ? 'active' : 'secondary'}`}
+          className={`paginator__button ${i === currentPage ? 'active' : 'secondary'}`}
           onClick={() => onPageSwitch(i, 0)}
-          disabled={i === page}
+          disabled={i === currentPage}
         >
           {i}
         </button>
@@ -50,28 +53,28 @@ const Paginator = ({ page, pages, listType }) => {
     return buttons;
   };
 
-  const centerButtons = renderCenterButtons(page, pages);
+  const centerButtons = renderCenterButtons(currentPage, totalPages);
 
   return (
     <div className="paginator">
       <button
         className="paginator__button paginator__button_mobile"
         onClick={() => onPageSwitch(1, 0)}
-        disabled={page <= 1}
+        disabled={currentPage <= 1}
       >
         {'<<'}
       </button>
 
       <button
         className="paginator__button paginator__button_prev"
-        onClick={() => onPageSwitch(page, -1)}
-        disabled={page <= 1}
+        onClick={() => onPageSwitch(currentPage, -1)}
+        disabled={currentPage <= 1}
       >
         {'<'}
       </button>
 
       <div
-        className={`paginator__limits ${page <= 3 || pages <= 5 ? 'hidden' : ''}`}
+        className={`paginator__limits ${currentPage <= 3 || totalPages <= 5 ? 'hidden' : ''}`}
       >
         <button
           className="paginator__button"
@@ -90,7 +93,7 @@ const Paginator = ({ page, pages, listType }) => {
       {centerButtons}
 
       <div
-        className={`paginator__limits ${pages <= 5 || page >= pages - 2 ? 'hidden' : ''}`}
+        className={`paginator__limits ${totalPages <= 5 || currentPage >= totalPages - 2 ? 'hidden' : ''}`}
       >
         <div className="paginator__dots">
           <div className="paginator__dots_dot" />
@@ -100,24 +103,24 @@ const Paginator = ({ page, pages, listType }) => {
 
         <button
           className="paginator__button"
-          onClick={() => onPageSwitch(pages, 0)}
+          onClick={() => onPageSwitch(totalPages, 0)}
         >
-          {pages}
+          {totalPages}
         </button>
       </div>
 
       <button
         className="paginator__button paginator__button_next"
-        onClick={() => onPageSwitch(page, 1)}
-        disabled={page >= pages}
+        onClick={() => onPageSwitch(currentPage, 1)}
+        disabled={currentPage >= totalPages}
       >
         {'>'}
       </button>
 
       <button
         className="paginator__button paginator__button_mobile"
-        onClick={() => onPageSwitch(pages, 0)}
-        disabled={page >= pages}
+        onClick={() => onPageSwitch(totalPages, 0)}
+        disabled={currentPage >= totalPages}
       >
         {'>>'}
       </button>
