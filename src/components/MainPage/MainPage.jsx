3dd33bc3aa12257334764dac39a12ca36eb0cd46
dumbus <dumbusx@gmail.com>
2024-12-28 12:14:22 +0300

    feat: add particles animation for MainPage

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -9,94 +9,100 @@ import newest from '../../assets/icons/main/newest.svg';
 import random from '../../assets/icons/main/random.svg';
 import search from '../../assets/icons/main/search.svg';
 
+import ParticlesBackground from '../ParticlesBackground/ParticlesBackground';
+
 const MainPage = () => {
   return (
     <div className="main-page container">
-      <div className="main-page__header title">
-        <img className="main-page__icon" src={main} alt="main-icon" />
-        <h1 className="main-page__title">Энциклопедия кино</h1>
-      </div>
+      <ParticlesBackground />
 
-      <div className="main-page__main">
-        <h2 className="main-page__subtitle">
-          Энциклопедия кино - Ваш интерактивный советник для совершения
-          путешествия в мир кинематографа!
-        </h2>
-
-        <div className="main-page__about">
-          <section className="main-page__selections main-page__section">
-            <h3 className="main-page__list_title title">
-              Вы можете получить различные подборки кинофильмов:
-            </h3>
-
-            <ul className="main-page__list">
-              <li className="main-page__list_item">
-                <img
-                  className="main-page__list_icon"
-                  src={popular}
-                  alt="popular"
-                />
-                <div className="main-page__list_text">Самые популярные</div>
-              </li>
-
-              <li className="main-page__list_item">
-                <img className="main-page__list_icon" src={best} alt="best" />
-                <div className="main-page__list_text">
-                  Самые высокооценённые
-                </div>
-              </li>
-
-              <li className="main-page__list_item">
-                <img
-                  className="main-page__list_icon"
-                  src={newest}
-                  alt="newest"
-                />
-                <div className="main-page__list_text">Самые свежие</div>
-              </li>
-            </ul>
-          </section>
-
-          <section className="main-page__random main-page__section">
-            <h3 className="main-page__list_title title">
-              Если хочется чего-то, но Вы не знаете, чего именно -
-              воспользуйтесь функцией броска кубика:
-            </h3>
-
-            <ul className="main-page__list">
-              <li className="main-page__list_item">
-                <img
-                  className="main-page__list_icon"
-                  src={random}
-                  alt="random"
-                />
-                <div className="main-page__list_text">Мне повезёт!</div>
-              </li>
-            </ul>
-          </section>
-
-          <section className="main-page__search main-page__section">
-            <h3 className="main-page__list_title title">
-              Если нужно получить информацию о каком-то конкретном фильме - Вы
-              всегда можете воспользоваться поиском по названию:
-            </h3>
-
-            <ul className="main-page__list">
-              <li className="main-page__list_item">
-                <img
-                  className="main-page__list_icon"
-                  src={search}
-                  alt="search"
-                />
-                <div className="main-page__list_text">Искать!</div>
-              </li>
-            </ul>
-          </section>
+      <div className="main-page__content">
+        <div className="main-page__header title">
+          <img className="main-page__icon" src={main} alt="main-icon" />
+          <h1 className="main-page__title">Энциклопедия кино</h1>
         </div>
-      </div>
 
-      <div className="main-page__footer">
-        Наслаждайтесь киномарафонами и открывайте для себя мир кинематографа!
+        <div className="main-page__main">
+          <h2 className="main-page__subtitle">
+            Энциклопедия кино - Ваш интерактивный советник для совершения
+            путешествия в мир кинематографа!
+          </h2>
+
+          <div className="main-page__about">
+            <section className="main-page__selections main-page__section">
+              <h3 className="main-page__list_title title">
+                Вы можете получить различные подборки кинофильмов:
+              </h3>
+
+              <ul className="main-page__list">
+                <li className="main-page__list_item">
+                  <img
+                    className="main-page__list_icon"
+                    src={popular}
+                    alt="popular"
+                  />
+                  <div className="main-page__list_text">Самые популярные</div>
+                </li>
+
+                <li className="main-page__list_item">
+                  <img className="main-page__list_icon" src={best} alt="best" />
+                  <div className="main-page__list_text">
+                    Самые высокооценённые
+                  </div>
+                </li>
+
+                <li className="main-page__list_item">
+                  <img
+                    className="main-page__list_icon"
+                    src={newest}
+                    alt="newest"
+                  />
+                  <div className="main-page__list_text">Самые свежие</div>
+                </li>
+              </ul>
+            </section>
+
+            <section className="main-page__random main-page__section">
+              <h3 className="main-page__list_title title">
+                Если хочется чего-то, но Вы не знаете, чего именно -
+                воспользуйтесь функцией броска кубика:
+              </h3>
+
+              <ul className="main-page__list">
+                <li className="main-page__list_item">
+                  <img
+                    className="main-page__list_icon"
+                    src={random}
+                    alt="random"
+                  />
+                  <div className="main-page__list_text">Мне повезёт!</div>
+                </li>
+              </ul>
+            </section>
+
+            <section className="main-page__search main-page__section">
+              <h3 className="main-page__list_title title">
+                Если нужно получить информацию о каком-то конкретном фильме - Вы
+                всегда можете воспользоваться поиском по названию:
+              </h3>
+
+              <ul className="main-page__list">
+                <li className="main-page__list_item">
+                  <img
+                    className="main-page__list_icon"
+                    src={search}
+                    alt="search"
+                  />
+                  <div className="main-page__list_text">Искать!</div>
+                </li>
+              </ul>
+            </section>
+          </div>
+        </div>
+
+        <div className="main-page__footer">
+          Наслаждайтесь киномарафонами и открывайте для себя мир кинематографа!
+        </div>
       </div>
     </div>
   );
---
+++ src/components/ParticlesBackground/ParticlesBackground.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useMemo, useState } from 'react';
+
+import Particles, { initParticlesEngine } from '@tsparticles/react';
+import { loadSlim } from '@tsparticles/slim';
+
+const ParticlesBackground = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const options = useMemo(
+    () => ({
+      background: {
+        color: { value: '#000000' }
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: { enable: true, mode: 'push' },
+          onHover: { enable: true, mode: 'repulse' }
+        },
+        modes: {
+          push: { quantity: 4 },
+          repulse: { distance: 200, duration: 0.4 }
+        }
+      },
+      particles: {
+        color: { value: '#ffffff' },
+        links: {
+          color: '#ffffff',
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1
+        },
+        move: {
+          enable: true,
+          speed: 6,
+          outModes: { default: 'bounce' }
+        },
+        number: {
+          value: 120,
+          density: { enable: true, area: 800 }
+        },
+        opacity: { value: 0.5 },
+        shape: { type: 'circle' },
+        size: { value: { min: 1, max: 5 } }
+      },
+      detectRetina: true
+    }),
+    []
+  );
+
+  return init ? <Particles id="tsparticles" options={options} /> : null;
+};
+
+export default ParticlesBackground;
